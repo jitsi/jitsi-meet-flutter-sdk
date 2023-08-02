c836bdeb7cff1abe77ee7b0b797fb62b55c09a95
Gabriel Borlea <gabriel.borlea@8x8.com>
2023-08-02 10:58:03 +0300

    feat: add only one export file (#3)
    * feat: add only one export file
    
    * Update pubspec.yaml

---
+++ lib/jitsi_meet_flutter_sdk.dart
@@ -0,0 +1,4 @@
+export 'src/jitsi_meet.dart' show JitsiMeet;
+export 'src/jitsi_meet_conference_options.dart' show JitsiMeetConferenceOptions;
+export 'src/jitsi_meet_event_listener.dart' show JitsiMeetEventListener;
+export 'src/jitsi_meet_user_info.dart';
