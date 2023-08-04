d2eed58a522377ea74a6fe696ef0b4d0ddc9f0fd
Gabriel Borlea <gabriel.borlea@8x8.com>
2023-08-04 13:20:30 +0300

    feat: add code documentation

--- lib/src/jitsi_meet.dart
+++ lib/src/jitsi_meet.dart
@@ -1,14 +1,17 @@
 import 'jitsi_meet_platform_interface.dart';
-
 import 'jitsi_meet_event_listener.dart';
 import 'jitsi_meet_conference_options.dart';
 import 'method_response.dart';
 
+/// The entry point for the sdk. It is used to launch the meeting screen, 
+/// to send and receive all the events.
 class JitsiMeet {
   Future<String?> getPlatformVersion() {
     return JitsiMeetPlatform.instance.getPlatformVersion();
   }
 
+  /// Joins a meeting with the given meeting [options] and 
+  /// optionally a [listener] is given for listening to events triggered by the native sdks.
   Future<MethodResponse> join(JitsiMeetConferenceOptions options, [JitsiMeetEventListener? listener]) async{
     return await JitsiMeetPlatform.instance.join(
         options,
@@ -16,38 +19,57 @@ class JitsiMeet {
     );
   }
 
+  /// The localParticipant leaves the current meeting.
   Future<MethodResponse> hangUp() async{
     return await JitsiMeetPlatform.instance.hangUp();
   }
 
+  /// Sets the state of the localParticipant audio [muted] according to the muted parameter.
   Future<MethodResponse> setAudioMuted(bool muted) async{
     return await JitsiMeetPlatform.instance.setAudioMuted(muted);
   }
 
+  /// Sets the state of the localParticipant video [muted] according to the muted parameter.
   Future<MethodResponse> setVideoMuted(bool muted) async{
     return await JitsiMeetPlatform.instance.setVideoMuted(muted);
   }
 
+  /// Sends a message via the data channel [to] one particular participant or to all of them. 
+  /// If the [to] param is empty, the [message] will be sent to all the participants in the conference.
+  /// 
+  /// In order to get the participantId for the [to] parameter, the [JitsiMeetEventListener.participantsJoined] 
+  /// event should be listened for, which have as a parameter the participantId and this should be stored somehow.
   Future<MethodResponse> sendEndpointTextMessage({String? to, required String message}) async {
     return await JitsiMeetPlatform.instance.sendEndpointTextMessage(to: to, message:message);
   }
 
+  /// Sets the state of the localParticipant screen sharing according to the [enabled] parameter.
   Future<MethodResponse> toggleScreenShare(bool enabled) async {
     return await JitsiMeetPlatform.instance.toggleScreenShare(enabled);
   }
 
+  /// Opens the chat dialog. If [to] contains a valid participantId, the private chat with that 
+  /// particular participant will be opened.
   Future<MethodResponse> openChat([String? to]) async {
     return await JitsiMeetPlatform.instance.openChat(to);
   }
 
+  /// Sends a chat message via [to] one particular participant or to all of them. 
+  /// If the [to] param is empty, the [message] will be sent to all the participants in the conference.
+  /// 
+  /// In order to get the participantId for the [to] parameter, the [JitsiMeetEventListener.participantsJoined] 
+  /// event should be listened for, which have as a parameter the participantId and this should be stored somehow.
   Future<MethodResponse> sendChatMessage({String? to, required String message}) async {
     return await JitsiMeetPlatform.instance.sendChatMessage(to: to, message:message);
   }
 
+  /// Closes the chat dialog.
   Future<MethodResponse> closeChat() async {
     return await JitsiMeetPlatform.instance.closeChat();
   }
 
+  /// Sends and event that will trigger the [JitsiMeetEventListener.participantsInfoRetrieved] event
+  /// which will contain participants information.
   Future<MethodResponse> retrieveParticipantsInfo() async {
     return await JitsiMeetPlatform.instance.retrieveParticipantsInfo();
   }
--- lib/src/jitsi_meet_event_listener.dart
+++ lib/src/jitsi_meet_event_listener.dart
@@ -1,28 +1,74 @@
 class JitsiMeetEventListener {
+  /// Called when a conference was joined.
+  /// 
+  /// [url] : the conference URL
   final Function(String url)? conferenceJoined;
 
+  /// Called when the active conference ends, be it because of user choice or because of a failure.
+  /// 
+  /// [url] : the conference URL
+  /// [error] : missing if the conference finished gracefully, otherwise contains the error message
   final Function(String url, Object? error)? conferenceTerminated;
 
+  /// Called before a conference is joined.
+  /// 
+  /// [url] : the conference URL
   final Function(String url)? conferenceWillJoin;
 
+  /// Called when a participant has joined the conference.
+  /// 
+  /// [email] : the email of the participant. It may not be set if the remote participant didn't set one.
+  /// [name] : the name of the participant.
+  /// [role] : the role of the participant.
+  /// [participantId] : the id of the participant.
   final Function(String? email, String? name, String? role, String? participantId)? participantJoined;
 
+  /// Called when a participant has left the conference.
+  /// 
+  /// [participantId] : the id of the participant that left.
   final Function(String? participantId)? participantLeft;
 
+  /// Called when the local participant's audio is muted or unmuted.
+  /// 
+  /// [muted] : a boolean indicating whether the audio is muted or not.
   final Function(bool muted)? audioMutedChanged;
 
+  /// Called when the local participant's video is muted or unmuted.
+  /// 
+  /// [muted] : a boolean indicating whether the video is muted or not.
   final Function(bool muted)? videoMutedChanged;
 
+  /// Called when an endpoint text message is received.
+  /// 
+  /// [senderId] : the id of the participant that sent the message.
+  /// [message] : the content of the message.
   final Function(String senderId, String message)? endpointTextMessageReceived;
 
+  /// Called when a participant starts or stops sharing his screen.
+  /// 
+  /// [participantId] : the id of the participant
+  /// [sharing] : the state of screen share
   final Function(String participantId, bool sharing)? screenShareToggled;
 
-  final Function(String senderId, String message, bool isPrivate)? chatMessageReceived;
+  /// Called when a chat text message is received.
+  /// 
+  /// [senderId] : the id of the participant that sent the message.
+  /// [message] : the content of the message.
+  /// [isPrivate] : `true` if the message is private, `false` otherwise.
+  /// [timestamp] : the (optional) timestamp of the message.
+  final Function(String senderId, String message, bool isPrivate, String? timestamp)? chatMessageReceived;
 
+  /// Called when the chat dialog is opened or closed.
+  /// 
+  /// [isOpen] : `true` if the chat dialog is open, `false` otherwise.
   final Function(bool isOpen)? chatToggled;
 
+  /// Called when `retrieveParticipantsInfo` action is called.
+  /// 
+  /// [participantsInfo] : a list of participants information as a string.
   final Function(String participantsInfo)? participantsInfoRetrieved;
 
+  /// Called when the SDK is ready to be closed. No meeting is happening at this point.
   final Function()? readyToClose;
 
   JitsiMeetEventListener({
