20c4dd3088025cfc982e495b36657d80b3f8d681
Gusakovsky, Sergey <sgusakovsky@qulix.com>
2025-01-16 10:31:56 +0300

    Removed url

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
   final _jitsiMeetPlugin = JitsiMeet();
 
   join() async {
-    final options = JitsiMeetConferenceOptions.fromUrl('https://meet.govar.online:9443/govar_speaking_club');
+    final options = JitsiMeetConferenceOptions.fromUrl('');
     var listener = JitsiMeetEventListener(
       conferenceJoined: (url) {
         debugPrint("conferenceJoined: url: $url");
