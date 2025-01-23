0e4125aac6fcc634f734e6899466b4e451cd9090
Gusakovsky, Sergey <sgusakovsky@qulix.com>
2025-01-23 13:03:20 +0300

    Updated versions for kotlin and gradle

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,198 +13,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool audioMuted = true;
-  bool videoMuted = true;
-  bool screenShareOn = false;
-  List<String> participants = [];
-  final _jitsiMeetPlugin = JitsiMeet();
-
-  join() async {
-    var listener = JitsiMeetEventListener(
-      conferenceJoined: (url) {
-        debugPrint("conferenceJoined: url: $url");
-      },
-      conferenceTerminated: (url, error) {
-        debugPrint("conferenceTerminated: url: $url, error: $error");
-      },
-      conferenceWillJoin: (url) {
-        debugPrint("conferenceWillJoin: url: $url");
-      },
-      participantJoined: (email, name, role, participantId) {
-        debugPrint(
-          "participantJoined: email: $email, name: $name, role: $role, "
-          "participantId: $participantId",
-        );
-        participants.add(participantId!);
-      },
-      participantLeft: (participantId) {
-        debugPrint("participantLeft: participantId: $participantId");
-      },
-      audioMutedChanged: (muted) {
-        debugPrint("audioMutedChanged: isMuted: $muted");
-      },
-      videoMutedChanged: (muted) {
-        debugPrint("videoMutedChanged: isMuted: $muted");
-      },
-      endpointTextMessageReceived: (senderId, message) {
-        debugPrint(
-            "endpointTextMessageReceived: senderId: $senderId, message: $message");
-      },
-      screenShareToggled: (participantId, sharing) {
-        debugPrint(
-          "screenShareToggled: participantId: $participantId, "
-          "isSharing: $sharing",
-        );
-      },
-      chatMessageReceived: (senderId, message, isPrivate, timestamp) {
-        debugPrint(
-          "chatMessageReceived: senderId: $senderId, message: $message, "
-          "isPrivate: $isPrivate, timestamp: $timestamp",
-        );
-      },
-      chatToggled: (isOpen) => debugPrint("chatToggled: isOpen: $isOpen"),
-      participantsInfoRetrieved: (participantsInfo) {
-        debugPrint(
-            "participantsInfoRetrieved: participantsInfo: $participantsInfo, ");
-      },
-      readyToClose: () {
-        debugPrint("readyToClose");
-      },
-      bottomViewTapped: () {
-        debugPrint("bottomViewTapped");
-      },
-      changeRoomTapped: () {
-        debugPrint("changeRoomTapped");
-      },
-      saveResultsTapped: () {
-        debugPrint("saveResultsTapped");
-      },
-      getTopicTapped: () {
-        debugPrint("getTopicTapped");
-      },
-    );
-    _jitsiMeetPlugin.join(listener);
-  }
-
-  hangUp() async {
-    await _jitsiMeetPlugin.hangUp();
-  }
-
-  setAudioMuted(bool? muted) async {
-    var a = await _jitsiMeetPlugin.setAudioMuted(muted!);
-    debugPrint("$a");
-    setState(() {
-      audioMuted = muted;
-    });
-  }
-
-  setVideoMuted(bool? muted) async {
-    var a = await _jitsiMeetPlugin.setVideoMuted(muted!);
-    debugPrint("$a");
-    setState(() {
-      videoMuted = muted;
-    });
-  }
-
-  sendEndpointTextMessage() async {
-    var a = await _jitsiMeetPlugin.sendEndpointTextMessage(message: "HEY");
-    debugPrint("$a");
-
-    for (var p in participants) {
-      var b =
-          await _jitsiMeetPlugin.sendEndpointTextMessage(to: p, message: "HEY");
-      debugPrint("$b");
-    }
-  }
-
-  toggleScreenShare(bool? enabled) async {
-    await _jitsiMeetPlugin.toggleScreenShare(enabled!);
-
-    setState(() {
-      screenShareOn = enabled;
-    });
-  }
-
-  openChat() async {
-    await _jitsiMeetPlugin.openChat();
-  }
-
-  sendChatMessage() async {
-    var a = await _jitsiMeetPlugin.sendChatMessage(message: "HEY1");
-    debugPrint("$a");
-
-    for (var p in participants) {
-      a = await _jitsiMeetPlugin.sendChatMessage(to: p, message: "HEY2");
-      debugPrint("$a");
-    }
-  }
-
-  closeChat() async {
-    await _jitsiMeetPlugin.closeChat();
-  }
-
-  retrieveParticipantsInfo() async {
-    var a = await _jitsiMeetPlugin.retrieveParticipantsInfo();
-    debugPrint("$a");
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Plugin example app'),
-          ),
-          body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  const SizedBox(
-                    width: double.infinity,
-                    height: 500,
-                    child: JitsiMeetWidget(room: 'https://meet.govar.online/govar_speaking_club'),
-                  ),
-                  TextButton(
-                    onPressed: join,
-                    child: const Text("Join"),
-                  ),
-                  TextButton(onPressed: hangUp, child: const Text("Hang Up")),
-                  Row(children: [
-                    const Text("Set Audio Muted"),
-                    Checkbox(
-                      value: audioMuted,
-                      onChanged: setAudioMuted,
-                    ),
-                  ]),
-                  Row(children: [
-                    const Text("Set Video Muted"),
-                    Checkbox(
-                      value: videoMuted,
-                      onChanged: setVideoMuted,
-                    ),
-                  ]),
-                  TextButton(
-                      onPressed: sendEndpointTextMessage,
-                      child: const Text("Send Hey Endpoint Message To All")),
-                  Row(children: [
-                    const Text("Toggle Screen Share"),
-                    Checkbox(
-                      value: screenShareOn,
-                      onChanged: toggleScreenShare,
-                    ),
-                  ]),
-                  TextButton(
-                      onPressed: openChat, child: const Text("Open Chat")),
-                  TextButton(
-                      onPressed: sendChatMessage,
-                      child: const Text("Send Chat Message to All")),
-                  TextButton(
-                      onPressed: closeChat, child: const Text("Close Chat")),
-                  TextButton(
-                      onPressed: retrieveParticipantsInfo,
-                      child: const Text("Retrieve Participants Info")),
-                ]),
-          )),
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 600,
+              child: JitsiMeetWidget(room: 'https://meet.govar.online/govar_speaking_club'),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 150,
+              child: Text('Bottom bar'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
